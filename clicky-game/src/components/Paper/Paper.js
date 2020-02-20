d6b17f2c225f7546ce28ec80ae96a0ffcca1a9c8
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2020-02-19 21:42:03 -0800

    yeehaw

---
+++ clicky-game/src/components/Paper/Paper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+const PaperGabby = props => (
+    <Paper>
+        {props.children}
+    </Paper>
+)
+
+export default PaperGabby;
