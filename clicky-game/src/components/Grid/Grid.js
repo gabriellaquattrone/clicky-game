bc57f4137a3bc0edc8d705d137fe823859013ddb
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2020-02-19 21:01:44 -0800

    Updates

--- clicky-game/src/components/Grid/Grid.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Grid from '@material-ui/core/Grid';
-
-const GridGabby = props =>
-    <Grid {...props}>
-        {props.children}
-    </Grid>
-
-export default GridGabby;
